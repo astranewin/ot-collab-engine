46543f63d365e2d81ecb3b58372c0b5dbfc9f8b9
astranewin <astranewin@gmail.com>
2026-03-29 20:59:07 +0500

    init commit

---
+++ src/main/java/astranewin/dev/realtime_collaborative_editor/RealTimeCollaborativeEditorApplication.java
@@ -0,0 +1,13 @@
+package astranewin.dev.realtime_collaborative_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeCollaborativeEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeCollaborativeEditorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/astranewin/dev/realtime_collaborative_editor/RealTimeCollaborativeEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package astranewin.dev.realtime_collaborative_editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeCollaborativeEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
